37ce73db035730d79d69615ec04b4e2f338063b3
sainaga <sainagakondapellyg60@gmail.com>
2026-02-17 22:13:52 +0530

    exceldemo

---
+++ javascriptP/githubcopilot.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+console.log("hello world")
+console.log("hello world")
+
+for(let i=1; i<=10; i++){
+    console.log(i);
+}
--- tests/excelDemo.js
+++ tests/excelDemo.js
@@ -1,7 +1,23 @@
-const ExcelJs=require('exceljs');
+const ExcelJs = require('exceljs');
+let rowN;
+let columnN;
+async function readExcel() {
+   const workbook = new ExcelJs.Workbook();
+   await workbook.xlsx.readFile("C:\\Users\\Sainaga.Kondapelly\\Downloads\\download.xlsx");
+   const worksheet = workbook.getWorksheet('Sheet1');
+   worksheet.eachRow((row, rownumber) => {
+      row.eachCell((cell, columnnumber) => {
+         if (cell.value == 'Apple') {
+            console.log(`Row ${rownumber}, Column ${columnnumber}: ${cell.value}`);
+            rowN = rownumber;
+            columnN = columnnumber;
+         }
+      })
+   })
 
-
-"C:\Users\saina\Downloads\download.xlsx"
-
+   worksheet.getCell(rowN, columnN).value = 'Sainaga';
+   await workbook.xlsx.writeFile("C:\\Users\\Sainaga.Kondapelly\\Downloads\\download.xlsx");
+}
+readExcel();
 
 
